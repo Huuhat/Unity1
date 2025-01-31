feb273042c1bd0fa7269d547553506571269b233
Huuhat <wjdehddud23@naver.com>
2025-01-31 19:19:24 +0900

    (2025.1.24) 사운드 시스템

--- Program/Assets/Class9th (Colllision)/Scripts/BilliardBall.cs
+++ Program/Assets/Class9th (Colllision)/Scripts/BilliardBall.cs
@@ -1,9 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class BilliardBall : MonoBehaviour
 {
+    private Rigidbody rigidbody;
+    [SerializeField] Vector3 direction;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        direction.Normalize();
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(direction, ForceMode.Force);
+
+
+        // ForceMode.Force : 연속적인 힘을 적용하는 방식이며, 객체에 힘을 계속적으로 전달하는
+        // 방법으로 시간에 비례하며 적용합니다.
+
+        // ForceMode.Impulse : 순간적인 힘을 적용하는 방식이며, 객채의 가속도가 아닌 속도로를 
+        // 변화시키는 방식입니다.
+
+        // ForceMode.VelocityChange : 순간적인 힘을 적용하는 방식이며, 객체의 속도만 변화시키며,
+        // 객체의 무게에는 영향을 받지 않습니다.
+
+        // ForceMode.Acceleration : 연속적인 힘을 적용하는 방식이며 ,객체의 힘을 계속적으로 전달하는
+        // 방법이지만, 객체의 무게에 받지 않습니다.
+    }
     private void OnCollisionEnter(Collision collision)
     {
         Debug.Log("OnCollisionEnter");
@@ -15,7 +52,34 @@ public class BilliardBall : MonoBehaviour
     }
     private void OnCollisionExit(Collision collision)
     {
-        Debug.Log("OnCollisionExit");
+        
+            Debug.Log("OnCollisionExit");
+        
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Goal Line")
+        {
+            Debug.Log("OnTriggerEnter");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Goal Line")
+        {
+            Debug.Log("OnTriggerStay");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Goal Line")
+        {
+            Debug.Log("OnTriggerExit");
+        }
     }
 }
+
+
